53ff590058c91a429de713c477f9a9fca0b44db7
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2021-04-21 15:09:17 +0200

    Update and rename Traffic_sign_classification-checkpoint-inference.py to Traffic_sign_classification-checkpoint-highinference.py

--- Traffic-sign-classification-microservices/2inference/Traffic_sign_classification-checkpoint-highinference.py
+++ Traffic-sign-classification-microservices/2inference/Traffic_sign_classification-checkpoint-highinference.py
@@ -1,7 +1,10 @@
+
+import time
 import cv2
 import glob
 import pickle
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import load_model
@@ -11,19 +14,31 @@ from tensorflow.keras.applications.resnet50 import preprocess_input, decode_pred
 #tf.disable_v2_behavior() 
 #tf.compat.v1.disable_resource_variables()
 
-model = load_model('traffic_sign_seq_highaccuracy.h5', compile = True)
-model.summary()
 
-file_name = "frame206.jpg"
+start = time.perf_counter()
+model = load_model('traffic_sign_seq_highaccuracy.h5', compile = True)
+'''
+model.load_weights('traffic_sign_seq_highaccuracy.h5', by_name=True)
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+			loss='categorical_crossentropy',
+			metrics=['accuracy'])
+'''
+model.summary()
+file_name = "frame203.jpg"#"frame206.jpg"#"Train/0/00000_00004_00029.png"
 IMG_SIZE = 32
 img_array = cv2.imread(file_name ,cv2.IMREAD_GRAYSCALE)  
 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE), interpolation = cv2.INTER_AREA)
 plt.imshow(new_array/255.0, cmap='gray')
 plt.show()
-
-#print (new_array.shape)
-new_array_2 = np.reshape(new_array,(-1,32,32, 1))
+print (new_array.shape)
+new_array_2=np.reshape(new_array,(-1,32,32, 1))
 
 #to predict single image
-class_prob = model.predict(new_array_2.T, batch_size=1)
+class_prob=model.predict(new_array_2.T,batch_size=1)
 print(class_prob)
+#classifications=model.predict_classes(new_array_2.T,batch_size=1)
+#print(classifications)
+
+elapsedTrain = time.perf_counter() - start
+print ('Train Time: %.3f seconds' % elapsedTrain)
